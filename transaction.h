63894721c18bc983567759831cf77a6d65aad768
Soham Banerjee <44441100sf@gmail.com>
2024-11-25 18:25:55 +0530

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -1,8 +1,27 @@
+// #ifndef TRANSACTION_H
+// #define TRANSACTION_H
+
+// #include <mysql/mysql.h>
+
+// void manageTransactions(MYSQL *conn);
+
+// #endif
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
 #include <mysql/mysql.h>
 
+
 void manageTransactions(MYSQL *conn);
 
-#endif
+
+void viewAllTransactions(MYSQL *conn);
+
+
+void searchTransactionsByDate(MYSQL *conn);
+
+
+void addTransaction(MYSQL *conn);
+
+#endif 
+
