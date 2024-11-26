a7b42e8c3b02c9d669c2855b5d3dac7f600d60d7
Jashandeep Singh <107495919+Jdsb06@users.noreply.github.com>
2024-11-26 19:02:19 +0530

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -1,27 +1,10 @@
-// #ifndef TRANSACTION_H
-// #define TRANSACTION_H
-
-// #include <mysql/mysql.h>
-
-// void manageTransactions(MYSQL *conn);
-
-// #endif
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
 #include <mysql/mysql.h>
 
-
 void manageTransactions(MYSQL *conn);
-
-
 void viewAllTransactions(MYSQL *conn);
-
-
 void searchTransactionsByDate(MYSQL *conn);
 
-
-void addTransaction(MYSQL *conn);
-
-#endif 
-
+#endif
