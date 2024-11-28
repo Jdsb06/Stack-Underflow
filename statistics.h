ca45a33e53d8982168fa349b4abe839a03713aa0
Jashandeep Singh <107495919+Jdsb06@users.noreply.github.com>
2024-11-28 20:56:52 +0530

    Update statistics.h

--- statistics.h
+++ statistics.h
@@ -3,6 +3,12 @@
 
 #include <mysql/mysql.h>
 
-void viewStatistics(MYSQL *conn);
+void manageStatistics(MYSQL *conn);
+void viewTotalItems(MYSQL *conn);
+void displayInventoryStock(MYSQL *conn);
+void generateInventoryTrendsGraph(int item_id);
+void generateTopSellingGraph();
+void generateTransactionsByTypeGraph();
+void viewLowStockAlerts(MYSQL *conn);
 
 #endif
